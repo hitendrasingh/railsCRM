ffaa69825246949bf6b350c0a581f3fb8129401b
Manoj <manoj.mk27@gmail.com>
2011-04-03 18:10:01 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniCRM::Application.config.secret_token = 'df3b1da7f096a437a7a15fe4f787ff424894412e6fd29a09f3ca03bb48f3abcc6c3875b0f78e8b26e17b035ad587e6d635cc2a26272037d0507974ddcc4e15db'
